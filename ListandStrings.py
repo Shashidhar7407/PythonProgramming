54ddf49c6a7f4a460771d342780950a42ed64d9d
Shashidhar7407 <shashank.papishetty@gmail.com>
2024-06-06 20:29:39 +0530

    latest code changes

--- ListandStrings.py
+++ ListandStrings.py
@@ -17,3 +17,33 @@ new_list="a".join(new_list)
 print(new_list)
 
 # we must make sure that sequene should not contin any non string values
+'''list_a = list(range(4))
+string_a = ",".join(list_a)
+print(string_a)'''
+
+
+# Negative indexing
+# we can also access the string by using negative indexing
+list_a=[1,2,3,4,5]
+print(list_a[::-1])
+
+# we can also access the items from the reverese by providing the negative index
+
+list_a=[1,2,3,4,5,6]
+print(list_a[-2])
+print(list_a[-3])
+
+
+#slicing with negative index
+
+print(list_a[-5:-1])
+
+
+#negative step index [start index should be greater than end index and range value shoudl be in negative]
+
+print(list_a[4:1:-1])
+print(list_a[2:4:-1]) #rrturns empty 
+
+name="Shashidhar Papishetty"
+name=name.upper()
+print(name[::-1])
